e08a5aaf8c15990f8b94bd543693a854c519e9dd
Patryk Prentki <patryk.prentki90@gmail.com>
2023-08-11 14:55:23 +0200

    Log in and log out code to the website

---
+++ seleniumpart1.py
@@ -0,0 +1,18 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+time.sleep(5)
+driver.find_element(By.NAME, 'username').send_keys('Admin')
+driver.find_element(By.NAME, 'password').send_keys('admin123')
+driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button').click()
+time.sleep(5)
+driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/span').click()
+driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/ul/li[4]/a').click()
+driver.quit()
+
+
